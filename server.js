b0452da53f72dd3f6de17d0d2302ebfea78f38eb
Emrah Güvenkaya <guvenkayaemrah52@gmail.com>
2021-12-06 21:43:28 +0300

    güncelleme notu: css kodları

---
+++ server.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+const express = require('express');
+const fetch = require('node-fetch');
+const htmlParser = require("node-html-parser");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+const genScript = (url) => {
+  let el = new htmlParser.HTMLElement("script", {});
+  el.innerHTML = `window.__ZortURL = "${url.replace(/"/g, "\\\"")}";`;
+};
+
+const importScript = () => {
+  
+};
+
+let Proxy = async (req, res) => {
+  console.log(`New request: ${req.originalUrl}`);
+  //if(req.query.passwd != process.env.PASSWD) return res.send("Invalid Password");
+  let url = req.query.url || "https://youtube.com/";
+  let f = await fetch(url, {
+    method: req.method,
+  });
+  if(f.headers.get('content-type').includes("html")){
+    const body = await f.text();
+    if(!htmlParser.valid(body)) {
+      console.warn(`[ERROR] Invalid HTML at ${url}`);
+      f.body.pipe(res);
+      return;
+    };
+    const root = htmlParser.parse(body);
+    let html = root.childNodes.filter((x) => x.tagName && x.tagName.toLowerCase() == "html")[0];
+    console.log("HTML", html); // is head??? what
+    
+    if(!html) {
+      console.warn(`[ERROR] No <html> at ${url}`);
+      res.send(body);
+      return;
+    };
+    
+    //let head = html.childNodes.filter((x) => x.tagName && x.tagName.toLowerCase() == "head")[0];
+    //console.log("HEAD", head);
+    
+    html.childNodes.unshift(importScript());
+    html.childNodes.unshift(genScript(url));
+    
+    
+    //fs.writeFileSync("./test.json", JSON.stringify(html, (_,v) => typeof(v) == "function" ? v.toString() : v, "\t"));
+  } else {
+    f.body.pipe(res);
+  };
+};
+
+
+const listener = (req, res) => {
+  if(!req.query.url) {
+    let o = {
+      "/": () => res.sendFile(__dirname + "/public/index.html"),
+      "/client.js": () => res.sendFile(__dirname + "/public/client.js"),
+    };
+    if(o[req.originalUrl]) o[req.originalUrl]();
+  } else {
+    Proxy(req, res);
+  };
+};
+
+app.all('/*', listener);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
